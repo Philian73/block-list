54105af4e7db6d1f469a267b15f12a0e992775e0
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-01-26 19:42:05 +0300

    Auth completed

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -3,8 +3,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
 import { CookieService } from './cookie.service';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import * as process from 'process';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, PasswordService, CookieService],
 })
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 (async function () {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
+
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 })();
