1864e197de5b98135b09bffa0a279b67941d9af3
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-01-27 05:26:09 +0300

    lint / format

--- packages/frontend/tailwind.config.ts
+++ packages/frontend/tailwind.config.ts
@@ -1,10 +1,9 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   plugins: [],
+  theme: {},
 };
+
 export default config;
