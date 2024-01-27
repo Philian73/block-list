e1cad6f2fda79c46c83444ab105c68244abb88c4
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-01-27 04:05:38 +0300

    block-list module completed

---
+++ packages/backend/src/block-list/block-list.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { DbService } from '../db/db.service';
+import { AddBlockItemDto, BlockItemDto, BlockListQueryDto } from './dto';
+
+@Injectable()
+export class BlockListService {
+  constructor(private dbService: DbService) {}
+
+  create(userId: number) {
+    return this.dbService.blockList.create({
+      data: {
+        ownerId: userId,
+      },
+    });
+  }
+
+  getByUser(userId: number, query: BlockListQueryDto) {
+    return this.dbService.blockList.findUniqueOrThrow({
+      where: { ownerId: userId },
+      include: {
+        items: {
+          where: { data: { contains: query.q, mode: 'insensitive' } as const },
+          orderBy: { createdAt: 'desc' } as const,
+        },
+      },
+    });
+  }
+
+  async addItem(userId: number, data: AddBlockItemDto) {
+    const blockList = await this.dbService.blockList.findUniqueOrThrow({
+      where: { ownerId: userId },
+    });
+
+    return this.dbService.blockItem.create({
+      data: { blockListId: blockList.id, ...data },
+    });
+  }
+
+  async removeItem(userId: number, itemId: number) {
+    const blockList = await this.dbService.blockList.findUniqueOrThrow({
+      where: { ownerId: userId },
+    });
+
+    return this.dbService.blockItem.delete({
+      where: {
+        blockListId: blockList.id,
+        id: itemId,
+      },
+    });
+  }
+}
