a83e4238a97fd5d538d40b3b2afa21f340c25326
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-02-09 21:19:48 +0300

    replaced 'a' to 'Link' from Next/Router

--- packages/frontend/src/widgets/header/ui/logo/logo.tsx
+++ packages/frontend/src/widgets/header/ui/logo/logo.tsx
@@ -1,10 +1,12 @@
 import { Icons } from '@/shared/assets/icons'
+import { ROUTES } from '@/shared/constants'
+import Link from 'next/link'
 
 export const Logo = () => {
   return (
-    <a className={'flex items-center gap-2 text-xl h-12'} href={''}>
+    <Link className={'flex items-center gap-2 text-xl h-12'} href={ROUTES.HOME}>
       <Icons.Shield className={'h-full w-max'} />
       <span>Easy Block</span>
-    </a>
+    </Link>
   )
 }
