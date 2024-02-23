45671bc33eca92f7d3fc260181813285e2b7137e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-02-23 20:51:30 +0300

    fix

--- packages/frontend/src/shared/ui/select/select.tsx
+++ packages/frontend/src/shared/ui/select/select.tsx
@@ -16,8 +16,11 @@ export type SelectOption = {
 }
 
 type SelectProps = {
+  className?: string
   errorMessage?: string
   label?: string
+  /** The name of the select. Submitted with its owning form as part of a name/value pair. */
+  name?: string
   /** The options to display.
    * {title: string, value: string, disabled?: boolean} */
   options: SelectOption[]
@@ -28,7 +31,7 @@ type SelectProps = {
    * Use it for setting `className` or `style`.
    */
   rootProps?: ComponentProps<'label'>
-} & ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
+} & ComponentPropsWithoutRef<typeof SelectPrimitive.Root>
 
 export const Select = forwardRef<ElementRef<typeof SelectPrimitive.Trigger>, SelectProps>(
   ({ className, errorMessage, label, options, placeholder, rootProps, ...rest }, ref) => {
@@ -38,15 +41,13 @@ export const Select = forwardRef<ElementRef<typeof SelectPrimitive.Trigger>, Sel
       <label {...rootProps} className={clsx('flex flex-col', rootProps?.className)}>
         {!!label && <span className={'mb-1'}>{label}</span>}
 
-        <SelectPrimitive.Root>
+        <SelectPrimitive.Root {...rest}>
           <SelectPrimitive.Trigger
             className={clsx(
               'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
               className
             )}
             ref={ref}
-            {...rest}
-            id={rootProps?.id}
           >
             <SelectPrimitive.Value placeholder={placeholder} />
             <SelectPrimitive.Icon asChild className={'h-4 w-4'}>
@@ -57,7 +58,7 @@ export const Select = forwardRef<ElementRef<typeof SelectPrimitive.Trigger>, Sel
           <SelectPrimitive.Portal>
             <SelectPrimitive.Content
               className={
-                'relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1'
+                'relative z-50 bg-white max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1'
               }
               position={'popper'}
             >
