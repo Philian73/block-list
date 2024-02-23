42feb31fd5e1198139d5cfce3415a8dfc8547089
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-02-23 22:39:23 +0300

    added SelectOption export

--- packages/frontend/src/shared/ui/index.ts
+++ packages/frontend/src/shared/ui/index.ts
@@ -1,4 +1,4 @@
 export { Button } from './button/button'
 export { PageSpinner } from './page-spinner/page-spinner'
-export { Select } from './select/select'
+export { Select, type SelectOption } from './select/select'
 export { TextField } from './text-field/text-field'
