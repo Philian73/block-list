b9865b7d1a7e4003b46f76a1701fd14cf798ac1d
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-02-23 23:47:43 +0300

    exports

--- packages/frontend/src/features/block-list/model/index.ts
+++ packages/frontend/src/features/block-list/model/index.ts
@@ -1,2 +1,3 @@
 export { type AddBlockListData, addBlockListSchema } from './add-block-list-schema'
 export { useAddBlockItemForm } from './use-add-block-item-form'
+export { useBlockItems } from './use-block-items'
