695a74ec7f762480198457a41b55ece8de7d371b
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-02-23 17:18:13 +0300

    exports

---
+++ packages/frontend/src/features/auth/model/index.ts
@@ -0,0 +1,5 @@
+export { type SignInData, signInSchema } from './sign-in-schema'
+export { type SignUpData, signUpSchema } from './sign-up-schema'
+export { useSignInForm } from './use-sign-in-form'
+export { useSignOut } from './use-sign-out'
+export { useSignUpForm } from './use-sign-up-form'
---
+++ packages/frontend/src/features/auth/ui/index.ts
@@ -0,0 +1,4 @@
+export { protectedPage } from './protected-page/protected-page'
+export { SignInForm } from './sign-in-form/sign-in-form'
+export { SignOut } from './sign-out/sign-out'
+export { SignUpForm } from './sign-up-form/sign-up-form'
