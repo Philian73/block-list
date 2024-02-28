f44c630555228faa711df9095a24bd368dbd5f00
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-02-28 17:14:27 +0300

    created base-api

---
+++ packages/extension/src/shared/api/base-api-instance.ts
@@ -0,0 +1,38 @@
+// custom-instance.ts
+
+class ApiError extends Error {
+  constructor(public data: unknown) {
+    super('Api Error')
+  }
+}
+
+const baseURL = 'http://localhost:3000' // use your own URL here or environment variable
+
+export const createInstance = async <T>({
+  data,
+  method,
+  params,
+  url,
+}: {
+  data?: BodyType<unknown>
+  method: 'DELETE' | 'GET' | 'PATCH' | 'POST' | 'PUT'
+  params?: Record<string, string>
+  responseType?: string
+  url: string
+}): Promise<T> => {
+  const response = await fetch(`${baseURL}${url}` + new URLSearchParams(params), {
+    method,
+    ...(data ? { body: JSON.stringify(data) } : {}),
+  })
+
+  if (response.status.toString().startsWith('2')) {
+    throw new ApiError(response)
+  }
+
+  return response.json()
+}
+
+export type BodyType<BodyData> = BodyData
+export type ErrorType<Error> = Error
+// In case you want to wrap the body type (optional)
+// (if the custom instance is processing data before sending it, like changing the case for example)
