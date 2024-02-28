8401bbe93f9718a7ce70b1dfd883eb5ab79f3229
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-02-28 17:54:06 +0300

    fix: 'https' to 'http'

--- packages/frontend/src/shared/api/base-api-instance.ts
+++ packages/frontend/src/shared/api/base-api-instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, type AxiosRequestConfig } from 'axios'
 
 export const baseApiInstance = axios.create({
-  baseURL: 'https://localhost:3000',
+  baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
   },
