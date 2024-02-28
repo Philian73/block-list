0e1411ae1612bd84917ff6eaca6071cd5846ab67
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-02-28 16:10:44 +0300

    added devDep lib @crxjs/vite-plugin@beta -D

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -1,11 +1,14 @@
 import * as path from 'path'
 
+import { crx } from '@crxjs/vite-plugin'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+import manifest from './manifest.json'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
