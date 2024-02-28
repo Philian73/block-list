c37a2cc0510613bcf2c0af4cc7e589721ef1bfea
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-02-28 17:57:33 +0300

    remove react-query devtool

--- packages/extension/src/app/providers/query-client-provider.tsx
+++ packages/extension/src/app/providers/query-client-provider.tsx
@@ -2,17 +2,11 @@ import type { ReactNode } from 'react'
 
 import { queryClient } from '@/shared/api'
 import { QueryClientProvider as TanStackQueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 type QueryClientProviderProps = {
   children: ReactNode
 }
 
 export const QueryClientProvider = ({ children }: QueryClientProviderProps) => {
-  return (
-    <TanStackQueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </TanStackQueryClientProvider>
-  )
+  return <TanStackQueryClientProvider client={queryClient}>{children}</TanStackQueryClientProvider>
 }
