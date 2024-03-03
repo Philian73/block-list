07b6b521ea673cb46bc4a8d5905431a8525e26ec
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-03 17:28:39 +0300

    www

---
+++ packages/extension/src/app/background.ts
@@ -0,0 +1,5 @@
+import { startUpdateBlockRules } from '@/features/add-block-rules/model'
+import { startToggleExtensionIcon } from '@/features/toggle-extension-icon/model'
+
+startUpdateBlockRules()
+startToggleExtensionIcon()
