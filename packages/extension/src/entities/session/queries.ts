ce54aa15900445ae4c38404d5297251a6190460e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-03 16:55:41 +0300

    deleted unused funcs

--- packages/extension/src/entities/session/queries.ts
+++ packages/extension/src/entities/session/queries.ts
@@ -1,49 +1,13 @@
-import {
-  authControllerGetSessionInfo,
-  authControllerSignIn,
-  authControllerSignOut,
-} from '@/shared/api/generated'
-import { ROUTES } from '@/shared/constants'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { useRouter } from 'next/router'
+import { authControllerGetSessionInfo } from '@/shared/api/generated'
+import { useQuery } from '@tanstack/react-query'
 
 const keys = {
   root: () => ['session'],
-  signIn: () => [...keys.root(), 'signIn'],
-  signOut: () => [...keys.root(), 'signOut'],
 }
 
 export const useSessionQuery = () => {
   return useQuery({
     queryFn: authControllerGetSessionInfo,
     queryKey: keys.root(),
-  })
-}
-
-export const useSignInMutation = () => {
-  const router = useRouter()
-  const queryClient = useQueryClient()
-
-  return useMutation({
-    mutationFn: authControllerSignIn,
-    mutationKey: keys.signIn(),
-    onSuccess() {
-      router.push(ROUTES.HOME)
-      queryClient.removeQueries({ queryKey: keys.root() })
-    },
-  })
-}
-
-export const useSignOutMutation = () => {
-  const router = useRouter()
-  const queryClient = useQueryClient()
-
-  return useMutation({
-    mutationFn: authControllerSignOut,
-    mutationKey: keys.signOut(),
-    onSuccess() {
-      router.push(ROUTES.SIGN_IN)
-      queryClient.removeQueries()
-    },
   })
 }
